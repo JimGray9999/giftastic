66e9f4668a7d81033a3bb6dfd8ee420d12b37671
Jim Gray <JimGray9999@users.noreply.github.com>
2017-07-07 00:20:59 -0400

    updates

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,43 +1,91 @@
 $(document).ready(function() {
-	
-	
-	// initial list of GIF searches to display
-	var searchGIFs = ["wilford brimley", "minions", "floor is lava", "fidget spinner", "why is the rum gone"];
-
-	// API call test
-	// API key: 8822962b66364b79b9f1e796900f3a96 
-	var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=wilford+brimley&api_key=8822962b66364b79b9f1e796900f3a96&limit=10");
-	xhr.done(function(data) { console.log("success got data", data); });
+	// initial list of GIF searches
+	var searchGIFs = ["wilford brimley", "minions", "floor is lava", 
+					  "fidget spinner", "why is the rum gone"];
 
 	// function to create buttons for each searchGIF choice
 	function createButtons() {	
+		$("#buttons").empty();
 
 		for (i = 0 ; i < searchGIFs.length ; i++){
 			
-			//add +'s to the button values, for API call searches
+			//add +'s to the button data-value, for API call searches
 			var dataValue = searchGIFs[i].replace(/\s+/g,"+");
 
 			//create a button for each string in the searchGIFs array
-			$("#buttons").append('<button class="btn btn-info" value="' + dataValue + '">' 
+			$("#buttons").append('<button class="btn btn-info show-gif" data-value="' + dataValue + '">' 
 			+ searchGIFs[i]  + '</button>');
-		}
+		};
+		console.log(searchGIFs);
 	};
 
-	createButtons();
+	function addButton() {
+
+		var dataValue = searchGIFs[searchGIFs.length - 1].replace(/\s+/g,"+");
+
+		$("#buttons").append('<button class="btn btn-info show-gif" data-value="' + dataValue + '">' 
+			+ searchGIFs[searchGIFs.length - 1]  + '</button>');
+
+		console.log(searchGIFs);
+	};
+
+	createButtons(); // create the initial buttons
 
 	//to do: click event - change the images to the button selected
 		// need the image, rating from the API call data returned
 		// display the next 10 GIFs
 		// data.rating, data[i].images.fixed_height and data[i].images.fixed_height_still
+		
+	$(".show-gif").on("click", function() {
+
+		$("#gifs").empty();
+
+		var search = $(this).attr("data-value");
+		console.log(search);
+		var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + search + "&api_key=8822962b66364b79b9f1e796900f3a96&limit=10";
+
+		$.ajax({
+	      url: queryURL,
+    	  method: "GET"
+    	}).done(function(response) {
+    		console.log(response);
+
+			for (i = 0 ; i < 10 ; i++){
+				
+				$("#gifs").append('<img src="' 
+					+ response.data[i].images.fixed_height_still.url 
+					+'" data-still="' + response.data[i].images.fixed_height_still.url
+					+ '"data-animate="' + response.data[i].images.fixed_height.url 
+					+ '" data-state="still" alt="gif-' + (i + 1) 
+					+ '" class="gif">');
+
+				$("#gifs").append('<p>Rating: ' + response.data[i].rating + '</p>');
+			};
+    	});
+	});
 
 	// to do: toggle event for clicking each image to start and stop GIF animation
 
-	//to do: function - add a new search parameter
-	$("#add-me").click(function() {
-		console.log(this);
+
+	//function - animate gif on click
+	$(".gif").on("click", function(){
+		var state = $(this).attr("data-state");
+		console.log(state);
+
+		if (state === "still"){
+			$(this).attr("src", $(this).attr("data-animate"));
+			$(this).attr("data-state", "animate");
+		} else {
+			$(this).attr("src", $(this).attr("data-still"));
+			$(this).attr("data-state", "still");
+		}
+	});
+
+	//function - add a new search button
+	$("#add-me").on("click", function() {
+		searchGIFs.push($("#new-search").val());
+		addButton();
 	});	
-		// accept the entered field
-		// add the phrase as a string to the searchGIFs array
-		// call the createButtons function to build buttons
+
 
 });
